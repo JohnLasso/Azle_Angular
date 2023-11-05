5f620a677c70cb60904fa95344d2e9b47663f767
Jacob Jäckel <jacob.jaeckel@hdi.de>
2023-11-05 21:12:50 +0100

    IC Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
+import { AuthClient } from '@dfinity/auth-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  authClient: AuthClient | null = null;
+
+  async ngOnInit(): Promise<void> {
+    this.authClient = await AuthClient.create({
+      idleOptions: {
+        idleTimeout: 1000 * 60 * 30, // set to 30 minutes
+        disableDefaultIdleCallback: true, // disable the default reload behavior
+      },
+    });
+  }
+
+  async login() {
+    await this.authClient!.login({
+      // 7 days in nanoseconds
+      maxTimeToLive: BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000),
+      onSuccess: async () => {
+        const identity = this.authClient!.getIdentity();
+        const principal = identity.getPrincipal().toString();
+        console.log(`Logged in as ${principal}`);
+        console.log(identity);
+      },
+    });
+  }
+
+  async logout() {
+    await this.authClient!.logout();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AuthClient } from "@dfinity/auth-client";
 
-import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      importProvidersFrom(
+        BrowserAnimationsModule,
+        BrowserModule,
+        AppRoutingModule
+      ),
+      
+    ],
+  });
+
+
+
